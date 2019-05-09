4417601f851e31e757973fa2686e04332db94db4
mandarin4452 <ckdgns0130@gmail.com>
2019-05-09 17:54:24 +0900

    login xml proto
    5/9 prototype

--- app/src/main/java/edu/skku/capstone/justpay/LoginActivity.java
+++ app/src/main/java/edu/skku/capstone/justpay/LoginActivity.java
@@ -1,7 +1,9 @@
 package edu.skku.capstone.justpay;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
 
 public class LoginActivity extends AppCompatActivity {
 
