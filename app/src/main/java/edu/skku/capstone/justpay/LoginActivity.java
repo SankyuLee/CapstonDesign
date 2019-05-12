a5f1c05277fce8e8828bd0b2646a7011d025874f
mandarin4452 <ckdgns0130@gmail.com>
2019-05-12 21:49:40 +0900

    Design / Function modified
    All functions except email validation & actionbar apperaing

--- app/src/main/java/edu/skku/capstone/justpay/LoginActivity.java
+++ app/src/main/java/edu/skku/capstone/justpay/LoginActivity.java
@@ -1,8 +1,12 @@
 package edu.skku.capstone.justpay;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import org.w3c.dom.Text;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -11,5 +15,22 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+
     }
+    public void onClick(View view) {
+        TextView b = findViewById(R.id.link_signup);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(),SignupActivity.class);
+                startActivity(intent);
+            }
+
+        });
+    }
+
+
 }
