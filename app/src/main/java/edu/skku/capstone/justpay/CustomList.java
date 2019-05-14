4ed75047a7e24ecdff4d2ace867a091514603a9c
L2zz <ulla4571@gmail.com>
2019-05-15 06:00:11 +0900

    Merge result into develop

--- app/src/main/java/edu/skku/capstone/justpay/CustomList.java
+++ app/src/main/java/edu/skku/capstone/justpay/CustomList.java
@@ -6,69 +6,125 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.BaseExpandableListAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import edu.skku.capstone.justpay.resultlist_item;
+public class CustomList extends BaseExpandableListAdapter {
+    Context mContext;
+    ArrayList<resultlist_item> personlists;
+    ArrayList<ArrayList<personal_item>> itemlists;
 
-public class CustomList extends BaseAdapter {
-    // Adapter에 추가된 데이터를 저장하기 위한 ArrayList
-    private ArrayList<resultlist_item> listViewItemList = new ArrayList<resultlist_item>() ;
-
-    // ListViewAdapter의 생성자
-    public CustomList() {
-
+    @Override
+    public int getGroupCount() {
+        return personlists.size();
     }
 
-    // Adapter에 사용되는 데이터의 개수를 리턴. : 필수 구현
     @Override
-    public int getCount() {
-        return listViewItemList.size() ;
+    public int getChildrenCount(int groupPosition) {
+        return itemlists.get(groupPosition).size();
     }
 
-    // position에 위치한 데이터를 화면에 출력하는데 사용될 View를 리턴. : 필수 구현
+    //리스트의 아이템 반환
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        final int pos = position;
-        final Context context = parent.getContext();
+    public resultlist_item getGroup(int groupPosition) {
 
-        // "listview_item" Layout을 inflate하여 convertView 참조 획득.
-        if (convertView == null) {
+        return personlists.get(groupPosition);
+    }
+
+    @Override
+    public personal_item getChild(int groupPosition, int childPosition) {
+        return itemlists.get(groupPosition).get(childPosition);
+    }
+
+    //리스트 아이템의 id 반환
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    //동일한 id가 항상 동일한 개체를 참조하는지 여부를 반환
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    //리스트 각각의 row에 view를 설정
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        View v = convertView;
+        Context context = parent.getContext();
+
+        //convertView가 비어있을 경우 xml파일을 inflate 해줌
+        if (v == null) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(R.layout.item, parent, false);
+            v = inflater.inflate(R.layout.item, parent, false);
         }
 
-        // 화면에 표시될 View(Layout이 inflate된)으로부터 위젯에 대한 참조 획득
-        TextView nameTextView = (TextView) convertView.findViewById(R.id.textView1) ;
-        TextView payTextView = (TextView) convertView.findViewById(R.id.textView2) ;
+        //View들은 반드시 아이템 레이아웃을 inflate한 뒤에 작성할 것
+        ImageView arrowIcon = (ImageView) v.findViewById(R.id.imageView);
+        TextView name = (TextView) v.findViewById(R.id.textView1);
+        TextView pay = (TextView) v.findViewById(R.id.textView2);
 
-        // Data Set(listViewItemList)에서 position에 위치한 데이터 참조 획득
-        resultlist_item listViewItem = listViewItemList.get(position);
+        //그룹 펼쳐짐 여부에 따라 아이콘 변경
+        if (getGroup(groupPosition).getName() == "오승민")
+            arrowIcon.setImageResource(R.drawable.green_circle_filled);
+        else
+           arrowIcon.setImageResource(R.drawable.empty_background);
 
-        // 아이템 내 각 위젯에 데이터 반영
-        nameTextView.setText(listViewItem.getName());
-        payTextView.setText(String.valueOf(listViewItem.getPay()));
-
-        return convertView;
+        //리스트 아이템의 내용 설정
+        name.setText(getGroup(groupPosition).getName());
+        pay.setText(String.valueOf(getGroup(groupPosition).getPay()));
+        //
+        return v;
     }
 
-    // 지정한 위치(position)에 있는 데이터와 관계된 아이템(row)의 ID를 리턴. : 필수 구현
     @Override
-    public long getItemId(int position) {
-        return position ;
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        View v = convertView;
+        Context context = parent.getContext();
+
+        if (v == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = inflater.inflate(R.layout.childitem, parent, false);
+        }
+
+        TextView name = (TextView) v.findViewById(R.id.textView1);
+        TextView pay = (TextView) v.findViewById(R.id.textView2);
+        TextView number = (TextView) v.findViewById(R.id.textView3);
+
+
+        name.setText(getChild(groupPosition, childPosition).getName());
+        pay.setText(String.valueOf(getChild(groupPosition, childPosition).getPay()));
+        number.setText(String.valueOf(getChild(groupPosition, childPosition).getNumber()));
+
+
+
+        return v;
     }
 
-    // 지정한 위치(position)에 있는 데이터 리턴 : 필수 구현
     @Override
-    public Object getItem(int position) {
-        return listViewItemList.get(position) ;
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
     }
 
-    public void addItem(String name, int pay) {
-        resultlist_item item = new resultlist_item(name,pay);
-
-        listViewItemList.add(item);
+    //리스트에 새로운 아이템을 추가
+    public void addItem(int groupPosition, personal_item item) {
+        itemlists.get(groupPosition).add(item);
     }
+    public void addPerson(resultlist_item item) {
+        personlists.add(item);
+    }
+    //리스트 아이템을 삭제
+    //public void removeChild(int groupPosition, int childPosition) {
+    //    childItems.get(groupPosition).remove(childPosition);
+    //}
 }
