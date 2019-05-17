d6afee02f6a0fe2a521a8b61c3a664f6bba236ca
SankyuLee <sklee7753@gmail.com>
2019-05-17 14:06:17 +0900

    Modify onClickListener of @id/link_signup

--- app/src/main/java/edu/skku/capstone/justpay/LoginActivity.java
+++ app/src/main/java/edu/skku/capstone/justpay/LoginActivity.java
@@ -35,10 +35,14 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
-    }
 
-    public void onClick(View view) {
-        Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
-        startActivity(intent);
+        TextView link_signup = findViewById(R.id.link_signup);
+        link_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
