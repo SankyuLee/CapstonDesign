9d756fd0a1b4296c1e8f3d54da48bd195183121e
SankyuLee <sklee7753@gmail.com>
2019-05-17 14:28:39 +0900

    Add javadoc comments in UserLoggedIn.java

--- app/src/main/java/edu/skku/capstone/justpay/UserLoggedIn.java
+++ app/src/main/java/edu/skku/capstone/justpay/UserLoggedIn.java
@@ -12,6 +12,10 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+/**
+ * 로그인한 사용자 정보를 담고 있는 static 클래스입니다. 생성하실 필요는 없고 해당 클래스의 LoginCheck(username, password),
+ * isLoggedIn(), getUser() 3개의 메소드를 이용하실 수 있습니다.
+ */
 @SuppressWarnings("unused")
 class UserLoggedIn {
     private static JSONObject result;
@@ -25,10 +29,21 @@ class UserLoggedIn {
 
     private UserLoggedIn() { }
 
+    /**
+     * 사용자의 로그인 여부를 boolean 값으로 반환하는 메소드입니다.
+     * @return 사용가가 로그인한 상태이면 true, 외에는 false를 반환합니다.
+     */
     boolean isLoggedIn() {
         return isLoggedIn;
     }
 
+    /**
+     * 로그인 화면의 필드에 입력한 아이디, 비밀번호를 웹 서버에 GET 방식으로 보내 검증합니다. 그리고 로그인 성공 여부를
+     * 반환합니다.
+     * @param username 아이디를 의미합니다.
+     * @param password 비밀번호를 의미합니다.
+     * @return 로그인 성공 여부를 boolean 값으로 반환합니다.
+     */
     static boolean LoginCheck(String username, String password) {
         Thread ct = new ConnectionTask(username, password);
         try {
@@ -57,6 +72,10 @@ class UserLoggedIn {
         return isLoggedIn;
     }
 
+    /**
+     * 기본 구조는 SQLSender의 ConnectionTask와 동일합니다. 다만 parameter로 아이디와 비밀번호를 받으며
+     * 상위 클래스에 의해 메소드가 호출되고 직접 사용하실 수 없습니다.
+     */
     private static class ConnectionTask extends Thread {
         String username;
         String password;
@@ -98,19 +117,28 @@ class UserLoggedIn {
         }
     }
 
+    /**
+     * 로그인한 사용자의 정보를 반환합니다. 따라서 로그인한 사용자의 정보를 다시 서버에 요청하실 필요 없이
+     * SQL문을 수월하게 작성하실 수 있습니다.
+     * @return 사용자가 로그인한 상태라면 사용자의 고유번호, 아이디, 비밀번호, 이메일, 핸드폰번호, 이름을 JSONObeject로 반환합니다.
+     * 사용자가 로그인하지 않았다면 null을 반환합니다. 따라서 NullPointerException에 대해 예외 처리를 해주셔야 합니다.
+     */
     static JSONObject getUser() {
-        JSONObject user = null;
-        try {
-             user = new JSONObject().put("id", id)
-                                   .put("username", username)
-                                   .put("password", password)
-                                   .put("email", email)
-                                   .put("phone", phone)
-                                   .put("name", name);
-        } catch (JSONException e) {
-            e.printStackTrace();
-            Log.e("Exception", "JSONException occurred in UserLoggedIn.java");
-        } finally {
+        if (!isLoggedIn) {
+            return null;
+        } else {
+            JSONObject user = null;
+            try {
+                user = new JSONObject().put("id", id)
+                        .put("username", username)
+                        .put("password", password)
+                        .put("email", email)
+                        .put("phone", phone)
+                        .put("name", name);
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Log.e("Exception", "JSONException occurred in UserLoggedIn.java");
+            }
             return user;
         }
     }
