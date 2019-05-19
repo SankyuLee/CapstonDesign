296b73c280e7e44ee00cc3e9aad38f0f4f0a722e
L2zz <ulla4571@gmail.com>
2019-05-19 20:41:15 +0900

    Implement initialization of room

--- app/src/main/java/edu/skku/capstone/justpay/RoomActivity.java
+++ app/src/main/java/edu/skku/capstone/justpay/RoomActivity.java
@@ -3,7 +3,7 @@ package edu.skku.capstone.justpay;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v4.content.ContextCompat;
+import android.graphics.Typeface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,7 +12,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -24,7 +24,7 @@ import java.util.ArrayList;
 
 public class RoomActivity extends AppCompatActivity{
 
-    private TextView RoomName, RoomID;
+    private TextView roomTitle, roomID;
     private ImageButton banBtn, addBtn, changeBtn;
 
     private RecyclerView tabListView;
@@ -33,7 +33,7 @@ public class RoomActivity extends AppCompatActivity{
     private ImageView receiptImg;
     private ImageButton prevReceiptBtn, nextReceiptBtn;
 
-    private TextView roomStatus1, roomStatus2, roomStatus3;
+    private TextView eventStatus1, eventStatus2, eventStatus3;
 
     private ListView chartItemListView;
     private RoomChartItemAdapter chartItemAdapter;
@@ -43,23 +43,38 @@ public class RoomActivity extends AppCompatActivity{
     private TextView typingStatus;
 
     private LinearLayout bottomContainer;
+    private EditText itemNameEdit, itemCostEdit, itemCountEdit;
+    private ImageButton addItemBtn;
     private Button confirmBtn;
+
+    private EventStatus eventStatus;
+    private enum EventStatus { MAKE_LIST,  PERSONAL_CHECK, CONFIRM_RESULT }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room);
 
+        roomID = findViewById(R.id.room_id);
+        roomTitle = findViewById(R.id.room_id);
         banBtn = findViewById(R.id.ban_user_btn);
         addBtn = findViewById(R.id.add_user_btn);
         changeBtn = findViewById(R.id.change_user_btn);
         tabListView = findViewById(R.id.room_tab_list);
+        receiptImg = findViewById(R.id.receipt_image);
+        prevReceiptBtn = findViewById(R.id.receipt_prev_btn);
+        nextReceiptBtn = findViewById(R.id.receipt_next_btn);
+        eventStatus1 = findViewById(R.id.event_status1);
+        eventStatus2 = findViewById(R.id.event_status2);
+        eventStatus3 = findViewById(R.id.event_status3);
         chartItemListView = findViewById(R.id.chart_list_view);
+        prevStatusBtn = findViewById(R.id.prev_status_btn);
+        nextStatusBtn = findViewById(R.id.next_status_btn);
+        typingStatus = findViewById(R.id.room_typing_status);
         bottomContainer = findViewById(R.id.room_bottom_container);
 
-        setTabs();
-        setChartItems();
-        setBottomContainer();
+        // Make Room with title, id, focused event name and room status
+        initRoom("5지조", "1234", "05/09 커피");
 
         banBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -108,32 +123,24 @@ public class RoomActivity extends AppCompatActivity{
                 builder.show();
             }
         });
-
-        confirmBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final CharSequence[] items = {"사용자별 결과 확인", "항목별 결과 확인"};
-
-                AlertDialog.Builder builder = new AlertDialog.Builder(RoomActivity.this);
-                builder.setTitle("결과 확인");
-                builder.setItems(items, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int pos) {  // pos 0: 사용자별 결과 확인, pos 1: 항목별 결과 확인
-                        Intent intent;
-                        intent = new Intent(RoomActivity.this, ResultActivity1.class);
-                        startActivity(intent);
-                    }
-                });
-                builder.show();
-            }
-        });
     }
 
-    private void setTabs() {
+    private void initRoom(String title, String id, String eventName) {
+        // Set room's title and id
+        roomTitle.setText(title);
+        roomID.setText(id);
+
+        // Init tabs and focusing target event
+        initTabs();
+        setTab(eventName);
+    }
+
+    private void initTabs() {
         LinearLayoutManager layoutManager =
                 new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         tabListView.setLayoutManager(layoutManager);
 
-        ArrayList<RoomTabItem> tabList = new ArrayList<>();
+        final ArrayList<RoomTabItem> tabList = new ArrayList<>();
         tabList.add(new RoomTabItem("05/09 커피"));
         tabList.add(new RoomTabItem("05/08 점심"));
 
@@ -141,7 +148,7 @@ public class RoomActivity extends AppCompatActivity{
             @Override
             public void onTabClicked(int position) {
                 /* Change Tab */
-                Toast.makeText(getApplicationContext(), position + "Clicked", Toast.LENGTH_SHORT).show();
+                setTab(tabList.get(position).getTabTitle());
             }
 
             @Override
@@ -156,20 +163,72 @@ public class RoomActivity extends AppCompatActivity{
         tabListView.addItemDecoration(tabDecoration);
     }
 
-    private void setChartItems() {
-        ArrayList<RoomChartItem> chartItemList = new ArrayList<>();
-        chartItemList.add(new RoomChartItem("커피", new Integer(3000), new Integer(2)));
-        chartItemList.add(new RoomChartItem("쿠키", new Integer(5400), new Integer(3)));
-        chartItemList.add(new RoomChartItem("설탕", new Integer(5400), new Integer(3)));
+    private void setTab(String eventName) {
+        Toast.makeText(getApplicationContext(), "Open " + eventName, Toast.LENGTH_SHORT).show();
+        // Get event info from db
+        int eventStatusInt = 0; // Status
+        ArrayList<RoomChartItem> itemList = new ArrayList<>(); // Item List
+        itemList.add(new RoomChartItem("커피", new Integer(3000), new Integer(2)));
+        itemList.add(new RoomChartItem("쿠키", new Integer(5400), new Integer(3)));
+        itemList.add(new RoomChartItem("설탕", new Integer(5400), new Integer(3)));
 
-        chartItemAdapter = new RoomChartItemAdapter(chartItemList);
+        // Set event status
+        switch (eventStatusInt) {
+            case 0:
+                eventStatus = EventStatus.MAKE_LIST;
+                eventStatus1.setTextColor(getResources().getColor(R.color.colorJustPay));
+                eventStatus1.setTypeface(eventStatus1.getTypeface(), Typeface.BOLD);
+                break;
+            case 1:
+                eventStatus = EventStatus.PERSONAL_CHECK;
+                eventStatus2.setTextColor(getResources().getColor(R.color.colorJustPay));
+                eventStatus2.setTypeface(eventStatus1.getTypeface(), Typeface.BOLD);
+                break;
+            case 2:
+                eventStatus = EventStatus.CONFIRM_RESULT;
+                eventStatus3.setTextColor(getResources().getColor(R.color.colorJustPay));
+                eventStatus3.setTypeface(eventStatus1.getTypeface(), Typeface.BOLD);
+                break;
+        }
+
+        // Set item chart
+        chartItemAdapter = new RoomChartItemAdapter(itemList);
         chartItemListView.setAdapter(chartItemAdapter);
+
+        // Set bottom container
+        setBottomContainer();
     }
 
     private void setBottomContainer() {
         LayoutInflater bottomInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        bottomInflater.inflate(R.layout.room_bottom_confirm, bottomContainer, true);
 
-        confirmBtn = findViewById(R.id.room_result_confirm_btn);
+        if (eventStatus == eventStatus.MAKE_LIST) {
+            bottomInflater.inflate(R.layout.room_bottom_insertion, bottomContainer, true);
+            itemNameEdit = findViewById(R.id.item_name_edit);
+            itemCostEdit = findViewById(R.id.item_cost_edit);
+            itemCountEdit = findViewById(R.id.item_count_edit);
+            addItemBtn = findViewById(R.id.add_item_btn);
+        } else {
+            bottomInflater.inflate(R.layout.room_bottom_confirm, bottomContainer, true);
+            confirmBtn = findViewById(R.id.room_result_confirm_btn);
+
+            confirmBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    final CharSequence[] items = {"사용자별 결과 확인", "항목별 결과 확인"};
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(RoomActivity.this);
+                    builder.setTitle("결과 확인");
+                    builder.setItems(items, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int pos) {  // pos 0: 사용자별 결과 확인, pos 1: 항목별 결과 확인
+                            Intent intent;
+                            intent = new Intent(RoomActivity.this, ResultActivity1.class);
+                            startActivity(intent);
+                        }
+                    });
+                    builder.show();
+                }
+            });
+        }
     }
 }
