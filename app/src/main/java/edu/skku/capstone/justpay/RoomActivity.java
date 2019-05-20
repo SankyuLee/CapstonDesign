ccd30ce83c49f2d2bc392072b0e55424310bd2cc
L2zz <ulla4571@gmail.com>
2019-05-21 00:21:45 +0900

    Implement functions related to receipt

--- app/src/main/java/edu/skku/capstone/justpay/RoomActivity.java
+++ app/src/main/java/edu/skku/capstone/justpay/RoomActivity.java
@@ -1,9 +1,16 @@
 package edu.skku.capstone.justpay;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Typeface;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,6 +27,10 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.io.File;
 import java.util.ArrayList;
 
 public class RoomActivity extends AppCompatActivity{
@@ -31,7 +42,8 @@ public class RoomActivity extends AppCompatActivity{
     private RoomTabAdapter tabAdapter;
 
     private ImageView receiptImg;
-    private ImageButton prevReceiptBtn, nextReceiptBtn;
+    private ImageButton addReceiptBtn, prevReceiptBtn, nextReceiptBtn;
+    private TextView addReceiptText, receiptStatus;
 
     private TextView eventStatus1, eventStatus2, eventStatus3;
 
@@ -42,7 +54,6 @@ public class RoomActivity extends AppCompatActivity{
 
     private TextView typingStatus;
 
-    private LinearLayout bottomContainer;
     private EditText itemNameEdit, itemCostEdit, itemCountEdit;
     private ImageButton addItemBtn;
     private Button confirmBtn;
@@ -50,10 +61,16 @@ public class RoomActivity extends AppCompatActivity{
     private EventStatus eventStatus;
     private enum EventStatus { MAKE_LIST,  PERSONAL_CHECK, CONFIRM_RESULT }
 
+    private ArrayList<File> receiptList;
+    private Integer curReceiptIndex;
+    private static final int PICK_FROM_ALBUM = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room);
+
+        getPermission();
 
         roomID = findViewById(R.id.room_id);
         roomTitle = findViewById(R.id.room_id);
@@ -62,6 +79,9 @@ public class RoomActivity extends AppCompatActivity{
         changeBtn = findViewById(R.id.change_user_btn);
         tabListView = findViewById(R.id.room_tab_list);
         receiptImg = findViewById(R.id.receipt_image);
+        addReceiptBtn = findViewById(R.id.add_receipt_btn);
+        addReceiptText = findViewById(R.id.add_receipt_text);
+        receiptStatus = findViewById(R.id.receipt_status);
         prevReceiptBtn = findViewById(R.id.receipt_prev_btn);
         nextReceiptBtn = findViewById(R.id.receipt_next_btn);
         eventStatus1 = findViewById(R.id.event_status1);
@@ -71,7 +91,6 @@ public class RoomActivity extends AppCompatActivity{
         prevStatusBtn = findViewById(R.id.prev_status_btn);
         nextStatusBtn = findViewById(R.id.next_status_btn);
         typingStatus = findViewById(R.id.room_typing_status);
-        bottomContainer = findViewById(R.id.room_bottom_container);
 
         // Make Room with title, id, focused event name and room status
         initRoom("5지조", "1234", "05/09 커피");
@@ -124,6 +143,35 @@ public class RoomActivity extends AppCompatActivity{
             }
         });
 
+        addReceiptBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addReceiptImg();
+            }
+        });
+
+        prevReceiptBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curReceiptIndex--;
+                if (curReceiptIndex < 0) {
+                    curReceiptIndex = 0;
+                }
+                setReceiptImg();
+            }
+        });
+
+        nextReceiptBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                curReceiptIndex++;
+                if (curReceiptIndex > receiptList.size()) {
+                    curReceiptIndex = receiptList.size();
+                }
+                setReceiptImg();
+            }
+        });
+
         prevStatusBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -149,6 +197,32 @@ public class RoomActivity extends AppCompatActivity{
                 }
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == PICK_FROM_ALBUM && data != null) {
+            Uri photoUri = data.getData();
+            Cursor cursor = null;
+            try {
+                String[] proj = {MediaStore.Images.Media.DATA};
+                assert photoUri != null;
+                cursor = getContentResolver().
+                        query(photoUri, proj, null, null, null);
+
+                assert cursor != null;
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                cursor.moveToFirst();
+
+                receiptList.add(new File(cursor.getString(column_index)));
+            } finally {
+                if (cursor != null) {
+                    cursor.close();
+                }
+            }
+        }
+        curReceiptIndex = receiptList.size() - 1;
+        setReceiptImg();
     }
 
     private void initRoom(String title, String id, String eventName) {
@@ -190,12 +264,21 @@ public class RoomActivity extends AppCompatActivity{
     private void setTab(String eventName) {
         Toast.makeText(getApplicationContext(), "Open " + eventName, Toast.LENGTH_SHORT).show();
         // Get event info from db
+        receiptList = new ArrayList<>(); // Receipt
         int eventStatusInt = 0; // Status
         ArrayList<RoomChartItem> itemList = new ArrayList<>(); // Item List
         itemList.add(new RoomChartItem("커피", new Integer(3000), new Integer(2)));
         itemList.add(new RoomChartItem("쿠키", new Integer(5400), new Integer(3)));
         itemList.add(new RoomChartItem("설탕", new Integer(5400), new Integer(3)));
         itemList.add(new RoomChartItem("공기", new Integer(100), new Integer(0)));
+
+        // Set receipt images
+        if (receiptList.size() > 0) {
+            curReceiptIndex = receiptList.size() - 1;
+        } else {
+            curReceiptIndex = 0;
+        }
+        setReceiptImg();
 
         // Set event status
         switch (eventStatusInt) {
@@ -326,6 +409,56 @@ public class RoomActivity extends AppCompatActivity{
                 prevStatusBtn.setBackgroundColor(getResources().getColor(R.color.colorJustPay));
                 nextStatusBtn.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
                 break;
+        }
+    }
+
+    private void addReceiptImg() {
+        goToAlbum();
+    }
+
+    private void getPermission() {
+        PermissionListener permissionListener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+
+            }
+
+            @Override
+            public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+
+            }
+        };
+
+        TedPermission.with(this)
+                .setPermissionListener(permissionListener)
+                .setRationaleMessage(getResources().getString(R.string.photo_permission2))
+                .setDeniedMessage(getResources().getString(R.string.photo_permission1))
+                .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA)
+                .check();
+    }
+
+    private void goToAlbum() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, PICK_FROM_ALBUM);
+    }
+
+    private void setReceiptImg() {
+        if (curReceiptIndex >= 0 && curReceiptIndex < receiptList.size()) {
+            receiptImg.setVisibility(View.VISIBLE);
+            addReceiptBtn.setVisibility(View.INVISIBLE);
+            addReceiptText.setVisibility(View.INVISIBLE);
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            Bitmap imgBm = BitmapFactory.
+                    decodeFile(receiptList.get(curReceiptIndex).getAbsolutePath(), options);
+            receiptImg.setImageBitmap(imgBm);
+            receiptStatus.setText(new Integer(curReceiptIndex + 1).toString() + " / " +
+                    new Integer(receiptList.size()).toString());
+        } else {
+            addReceiptBtn.setVisibility(View.VISIBLE);
+            addReceiptText.setVisibility(View.VISIBLE);
+            receiptImg.setVisibility(View.INVISIBLE);
+            receiptStatus.setText("+ / " + new Integer(receiptList.size()).toString());
         }
     }
 }
