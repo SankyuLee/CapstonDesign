7c53b724815f8e275f5ea1215dfc68f52b569781
mandarin4452 <ckdgns0130@gmail.com>
2019-05-21 21:44:37 +0900

    Signup Validation
    Email,Nickname, Phone-number check
    
    Phone number input container modify

--- app/src/main/java/edu/skku/capstone/justpay/SignupActivity.java
+++ app/src/main/java/edu/skku/capstone/justpay/SignupActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.telephony.PhoneNumberFormattingTextWatcher;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Patterns;
@@ -22,9 +23,12 @@ import java.util.regex.Pattern;
 
 public class SignupActivity extends AppCompatActivity {
     private static final String TAG = "SignupActivity";
-    EditText et_pw, et_pw_chk;
+    EditText et_pw, et_pw_chk, et_ph;
     TextView s_em, s_pw, s_pw_chk;
 
+    int bit1 = 1;
+    int bit2 = 1;
+    int bit3 = 1;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +38,10 @@ public class SignupActivity extends AppCompatActivity {
         Toolbar tb = findViewById(R.id.signup_toolbar);
         setSupportActionBar(tb);
         getSupportActionBar().setDisplayShowTitleEnabled(true);
+
+
+        et_ph = findViewById(R.id.input_phone);
+        et_ph.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
 
 
 
@@ -58,10 +66,13 @@ public class SignupActivity extends AppCompatActivity {
                 if(isValidPassword(text)) {
                     s_pw.setText("올바른 비밀번호 형식입니다");
                     s_pw.setTextColor(Color.BLACK);
+                    bit1 = 0;
+
                 }
                 else{
                     s_pw.setText("올바르지 못한 비밀번호 형식입니다");
                     s_pw.setTextColor(Color.RED);
+                    bit1 = 1;
                 }
             }
         });
@@ -77,10 +88,12 @@ public class SignupActivity extends AppCompatActivity {
                 if(text1.equals(text2)){
                     s_pw_chk.setText("비밀번호가 일치합니다");
                     s_pw_chk.setTextColor(Color.BLACK);
+                    bit2 = 0;
                 }
                 else{
                     s_pw_chk.setText("비밀번호가 일치하지 않습니다");
                     s_pw_chk.setTextColor(Color.RED);
+                    bit2 = 1;
                 }
             }
             @Override
@@ -103,11 +116,13 @@ public class SignupActivity extends AppCompatActivity {
                 if (Patterns.EMAIL_ADDRESS.matcher(text).matches()){
                     EmailOutput.setText("올바른 이메일 형식입니다");
                     EmailOutput.setTextColor(Color.BLACK);
+                    bit3 = 0;
 
                 }
                 else {
                     EmailOutput.setText("올바른 이메일 형식이 아닙니다");
                     EmailOutput.setTextColor(Color.RED);
+                    bit3 = 1;
                 }
             }
 
@@ -128,32 +143,39 @@ public class SignupActivity extends AppCompatActivity {
         btn2.setOnClickListener(new Button.OnClickListener(){
             public void onClick(View v){
                 //입력한 항목 모두 검증
+                if (bit1 == 0 && bit2 == 0 && bit3 ==0) {
+                    String email = ((EditText) findViewById(R.id.input_email)).getText().toString();
+                    String pw = et_pw.getText().toString();
+                    String nickname = ((EditText) findViewById(R.id.input_nickname)).getText().toString();
+                    String phone = ((EditText) findViewById(R.id.input_phone)).getText().toString();
 
-                String email = ((EditText) findViewById(R.id.input_email)).getText().toString();
-                String pw = et_pw.getText().toString();
-                String nickname = ((EditText) findViewById(R.id.input_nickname)).getText().toString();
-                String phone = ((EditText) findViewById(R.id.input_phone)).getText().toString();
+                    JSONObject sql_result = new SQLSender().sendSQL("INSERT into users(email, password, phone, nickname) values('"
+                            +email+"','"+pw+"','"+phone+"','"+nickname+"');");
 
-                JSONObject sql_result = new SQLSender().sendSQL("INSERT into users(email, password, phone, nickname) values('"
-                +email+"','"+pw+"','"+phone+"','"+nickname+"');");
-
-                try {
-                    if (sql_result.getBoolean("isError")) {
-                        if (sql_result.getJSONObject("result").getString("code").equals(Constant.ER_DUP_ENTRY)) {
-                            EmailOutput.setText("이메일이 중복됩니다.");
-                            EmailOutput.setTextColor(Color.RED);
+                    try {
+                        if (sql_result.getBoolean("isError")) {
+                            if (sql_result.getJSONObject("result").getString("code").equals(Constant.ER_DUP_ENTRY)) {
+                                EmailOutput.setText("이메일이 중복됩니다.");
+                                EmailOutput.setTextColor(Color.RED);
+                            } else {
+                                // 알 수 없는 오류
+                                Toast.makeText(SignupActivity.this, "알 수 없는 오류가 발생하였습니다."+sql_result.getJSONObject("result"), Toast.LENGTH_LONG).show();
+                            }
                         } else {
-                            // 알 수 없는 오류
-                            Toast.makeText(SignupActivity.this, "알 수 없는 오류가 발생하였습니다."+sql_result.getJSONObject("result"), Toast.LENGTH_LONG).show();
+                            // 회원가입 성공
+                            Toast.makeText(SignupActivity.this, "회원가입에 성공하였습니다. 다시 로그인해 주세요.", Toast.LENGTH_LONG).show();
+                            finish();
                         }
-                    } else {
-                        // 회원가입 성공
-                        Toast.makeText(SignupActivity.this, "회원가입에 성공하였습니다. 다시 로그인해 주세요.", Toast.LENGTH_LONG).show();
-                        finish();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
-                } catch (JSONException e) {
-                    e.printStackTrace();
                 }
+                else {
+                    Toast toast =Toast.makeText(getApplicationContext(),"항목을 모두 올바르게 입력해주세요",Toast.LENGTH_LONG);
+                    toast.show();
+                }
+
+
             }
         });
     }
