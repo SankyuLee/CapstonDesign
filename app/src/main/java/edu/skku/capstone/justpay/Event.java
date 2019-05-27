27c3321bfabc24c8780ab6d23e779fac097ab468
L2zz <ulla4571@gmail.com>
2019-05-28 01:10:46 +0900

    Modify Event class
    Add result and payer

--- app/src/main/java/edu/skku/capstone/justpay/Event.java
+++ app/src/main/java/edu/skku/capstone/justpay/Event.java
@@ -2,14 +2,17 @@ package edu.skku.capstone.justpay;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Event {
 
     private Integer eventId;                            // 이벤트 아이디
     private String eventTitle;                          // 제목
-    private Member eventManager;                        // 관리인 아이디
+    private Member eventManager;                        // 관리인
+    private Member eventPayer;                          // 지불자
     private ArrayList<File> receiptList;                // 등록된 영수증 목록
     private ArrayList<RoomChartItem> chartItems;        // 등록된 항목 목록
+    private HashMap<Integer, Integer> chartResult;      // 사용자의 항목별 참여 결과 <항목 이름, 수량>
     private int eventStatus;                            // 상태
 
     // 이벤트의 상태를 나타내는 상수
@@ -17,20 +20,25 @@ public class Event {
     public static final int PERSONAL_CHECK = 1;
     public static final int CONFIRM_RESULT = 2;
 
-    public Event() {
+    public Event(Integer eventId, String eventTitle) {
         this.eventManager = null;
-        this.receiptList = null;
-        this.chartItems = null;
+        this.eventPayer = null;
+        this.receiptList = new ArrayList<>();
+        this.chartItems = new ArrayList<>();
+        this.chartResult = new HashMap<>();
         this.eventStatus = MAKE_LIST;
     }
 
-    public Event(Integer eventId, String eventTitle, Member eventManager,
-                 ArrayList<File> receiptList, ArrayList<RoomChartItem> chartItems, int eventStatus) {
+    public Event(Integer eventId, String eventTitle, Member eventManager, Member eventPayer,
+                 ArrayList<File> receiptList, ArrayList<RoomChartItem> chartItems,
+                 HashMap<Integer, Integer> chartResult, int eventStatus) {
         this.eventId = eventId;
         this.eventTitle = eventTitle;
         this.eventManager = eventManager;
+        this.eventPayer = eventPayer;
         this.receiptList = receiptList;
         this.chartItems = chartItems;
+        this.chartResult = chartResult;
         this.eventStatus = eventStatus;
     }
 
@@ -80,5 +88,21 @@ public class Event {
 
     public void setEventStatus(int eventStatus) {
         this.eventStatus = eventStatus;
+    }
+
+    public HashMap<Integer, Integer> getChartResult() {
+        return chartResult;
+    }
+
+    public void setChartResult(HashMap<Integer, Integer> chartResult) {
+        this.chartResult = chartResult;
+    }
+
+    public Member getEventPayer() {
+        return eventPayer;
+    }
+
+    public void setEventPayer(Member eventPayer) {
+        this.eventPayer = eventPayer;
     }
 }
