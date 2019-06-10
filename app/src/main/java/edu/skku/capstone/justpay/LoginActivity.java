84cdebfdaec4385a799119ae96eee85f854bdaa4
SeungMin-Oh <ohsm1008@naver.com>
2019-06-10 23:03:38 +0900

    Error checking

--- app/src/main/java/edu/skku/capstone/justpay/LoginActivity.java
+++ app/src/main/java/edu/skku/capstone/justpay/LoginActivity.java
@@ -3,8 +3,13 @@ package edu.skku.capstone.justpay;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -20,6 +25,8 @@ import com.kakao.auth.Session;
 import com.kakao.util.exception.KakaoException;
 import com.kakao.util.helper.log.Logger;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -37,6 +44,19 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+         /*   try {
+                PackageInfo info = getPackageManager().getPackageInfo("edu.skku.capstone.justpay", PackageManager.GET_SIGNATURES);
+                for (Signature signature : info.signatures) {
+                    MessageDigest md = MessageDigest.getInstance("SHA");
+                    md.update(signature.toByteArray());
+                    Log.e("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+                }
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }*/
+
 
         appData = getSharedPreferences("appData",MODE_PRIVATE);
         saveLoginData = appData.getBoolean("SAVE_LOGIN_DATA",false);
